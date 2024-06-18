30cfa9fd0a74718be8fdd3858eb81677fb3d4ec2
Shuduo Sang <sangshuduo@gmail.com>
2024-06-18 18:19:52 -0400

    fix: remove debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Unofficial tpl.ca app',
       theme: ThemeData(
         // This is the theme of your application.
